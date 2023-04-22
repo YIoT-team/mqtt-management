56d5635360c263abb04aec30a5f6c839a04ac8ed
Roman Kutashenko <kutashenko@gmail.com>
2023-04-22 11:57:18 +0300

    Add build scripts

---
+++ scripts/docker-build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#  ────────────────────────────────────────────────────────────
+#                     ╔╗  ╔╗ ╔══╗      ╔════╗
+#                     ║╚╗╔╝║ ╚╣╠╝      ║╔╗╔╗║
+#                     ╚╗╚╝╔╝  ║║  ╔══╗ ╚╝║║╚╝
+#                      ╚╗╔╝   ║║  ║╔╗║   ║║
+#                       ║║   ╔╣╠╗ ║╚╝║   ║║
+#                       ╚╝   ╚══╝ ╚══╝   ╚╝
+#    ╔╗╔═╗                    ╔╗                     ╔╗
+#    ║║║╔╝                   ╔╝╚╗                    ║║
+#    ║╚╝╝  ╔══╗ ╔══╗ ╔══╗  ╔╗╚╗╔╝  ╔══╗ ╔╗ ╔╗╔╗ ╔══╗ ║║  ╔══╗
+#    ║╔╗║  ║║═╣ ║║═╣ ║╔╗║  ╠╣ ║║   ║ ═╣ ╠╣ ║╚╝║ ║╔╗║ ║║  ║║═╣
+#    ║║║╚╗ ║║═╣ ║║═╣ ║╚╝║  ║║ ║╚╗  ╠═ ║ ║║ ║║║║ ║╚╝║ ║╚╗ ║║═╣
+#    ╚╝╚═╝ ╚══╝ ╚══╝ ║╔═╝  ╚╝ ╚═╝  ╚══╝ ╚╝ ╚╩╩╝ ║╔═╝ ╚═╝ ╚══╝
+#                    ║║                         ║║
+#                    ╚╝                         ╚╝
+#
+#    Lead Maintainer: YIoT team
+#  ────────────────────────────────────────────────────────────
+
+set -e
+
+SCRIPT_PATH="$(cd $(dirname "$0") >/dev/null 2>&1 && pwd)"
+PROJECT_PATH="${SCRIPT_PATH}/../ext/management-center"
+
+. ${SCRIPT_PATH}/SETTINGS
+
+pushd "${PROJECT_PATH}"
+
+  #   Install deps
+  rm -f yarn.lock
+  yarn install
+
+  #   Clean artifacts if present
+  rm -rf backend/public || true
+  rm -f yarn.lock
+
+  #   Buils with base path
+  yarn workspace @cedalo/management-center-frontend run build-with-base-path
+
+  #   Move artifacts to a path required by Dockerfile
+  pushd frontend
+    mv ../backend/public build 
+  popd
+
+  #
+  #   Build docker image
+  #
+  sudo docker build --build-arg CEDALO_MC_BUILD_DATE="$(date)" --build-arg CEDALO_MC_BUILD_NUMBER="$(date '+%s')" -t ${DOCKER_IMAGE0} -t ${DOCKER_IMAGE1} .
+popd
